1b98fcb2499f9fd43c4a75555419869d76206722
José Wesley Toledo <76110325+wesleytoledo05@users.noreply.github.com>
2022-06-27 16:26:35 -0300

    .

--- src/index.js
+++ src/index.js
@@ -20,9 +20,14 @@ app.use(session({
   saveUninitialized: true
 }));
 
-router.get('/', async(request, response) => {
-   response.status(200).json({ message: "Bem vindo a API da Clinica Oasis!" })
-})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 
 require('./controllers/authController')(app);
 require('./controllers/customerController')(app);
